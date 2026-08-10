6df0dab019815d1af02536b9220b65ec40219ef8
Team Pulse <ri8502744@gmail.com>
2026-08-11 04:40:52 +0600

    Deploy attrition-score prototype picker

--- packages/core/src/index.ts
+++
@@ -1,7 +0,0 @@
-export * from './money.js';
-export * from './dates.js';
-export * from './types.js';
-export * from './payroll.js';
-export * from './leave.js';
-export * from './attrition.js';
-export * from './subscription.js';
